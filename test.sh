f2cdac6bbf8f4755b6f43599db286b879768f425
Dilawar Singh <dilawars@ncbs.res.in>
2018-11-19 20:26:27 +0530

    Enabled tests for tsv scripts.

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 set -e 
-set -x
 
 # virtualenv does not require --user
 PYTHON=$(which python)
@@ -14,8 +13,14 @@ find . -type f -name "*.py" | xargs -I file $PYTHON -m pylint \
     --exit-zero \
     -E file
 
+# Run it in ./TestTSV directory.
+for _tsv in $(find ./TestTSV -name *.tsv -type f); do
+    $PYTHON findSim.py ${_tsv} --model models/synSynth7.g
+done
+
 $PYTHON findSim.py ./Curated/FindSim-Jain2009_Fig4F.tsv --model models/synSynth7.g
 $PYTHON findSim.py ./Curated/FindSim-Bhalla1999_fig2B.tsv --model models/synSynth7.g
-$PYTHON findSim.py ./Curated/FindSim-Gu2004_fig3B.tsv --model models/synSynth7.g
+# Following takes a lot of time to run.
+#$PYTHON findSim.py ./Curated/FindSim-Gu2004_fig3B.tsv --model models/synSynth7.g
 $PYTHON findSim.py ./Curated/FindSim-Ji2010_fig1C_ERK_acute.tsv --model models/synSynth7.g
 $PYTHON findSim.py ./Curated/FindSim-Bhalla1999_fig4C.tsv --model models/synSynth7.g
