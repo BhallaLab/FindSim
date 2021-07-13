4acb77d3bf520ab8ac58edbe775932fb35068a77
hrani <ranishashi@gmail.com>
2021-07-14 03:20:29 +0530

    git action

--- test.sh
+++ test.sh
@@ -6,12 +6,8 @@ set -x
 # virtualenv does not require --user
 PYTHON=$(which python)
 
-find . -type f -name "*.py" | xargs -I file $PYTHON -m pylint \
-    --disable=no-member \
-    --exit-zero \
-    -E file
 
 $PYTHON findSim.py ./Curated/Jain2009_Fig3F.json --model models/synSynth7.g
 $PYTHON findSim.py ./Curated/Bhalla1999_fig2B.json --model models/synSynth7.g
-$PYTHON findSim.py ./Curated/Gu2004_fig3B.json --model models/synSynth7.g
-$PYTHON findSim.py ./Curated/Bhalla1999_fig4C.json --model models/synSynth7.g
+$PYTHON findSim.py ./Curated/Gu2004_Fig3B.json --model models/synSynth7.g
+$PYTHON findSim.py ./Curated/Bhalla1999_Fig4C.json --model models/synSynth7.g
