ac9a41bc86bab00d6d0046bc6184654cadccdf78
hrani <ranishashi@gmail.com>
2021-07-14 17:46:52 +0530

    corrected filename

--- test.sh
+++ test.sh
@@ -8,5 +8,5 @@ PYTHON=$(which python)
 
 $PYTHON findSim.py ./Curated/Jain2009_Fig3F.json --model models/synSynth7.g --map models/synSynth7_map.json
 $PYTHON findSim.py ./Curated/Bhalla1999_Fig2B.json --model models/synSynth7.g --map models/synSynth7_map.json
-$PYTHON findSim.py ./Curated/Gu2004_Fig3B.json --model models/synSynth7.g --map models/synSynth7_map.json
+$PYTHON findSim.py ./Curated/Gu2004_fig3B.json --model models/synSynth7.g --map models/synSynth7_map.json
 $PYTHON findSim.py ./Curated/Bhalla1999_Fig4C.json --model models/synSynth7.g --map models/synSynth7_map.json
